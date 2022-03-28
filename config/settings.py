6f15d186b523ffb40d4eef0a031177d8052589dd
Suraj K Thapa <thapasurajk@gmail.com>
2022-03-28 17:23:06 +1100

    heroku hosting

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-tl^z9^v6zd432$jpa*2a$q8(o)v5no+9yk2viss*axr#$snubj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
